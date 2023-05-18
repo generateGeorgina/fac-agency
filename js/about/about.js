7b162ac5f3636bb2d73dd8519ac25fbcc5bd1e19
Matthew Powell <matt.powell2784@gmail.com>
2023-05-18 20:16:13 +0100

    add about list

---
+++ js/about/about.js
@@ -0,0 +1,17 @@
+const aboutList = [
+  {
+    key: 1,
+    image: '/img/home.svg',
+    text: 'Home',
+  },
+  {
+    key: 2,
+    image: '/img/home.svg',
+    text: 'Home',
+  },
+  {
+    key: 3,
+    image: '/img/home.svg',
+    text: 'Home',
+  },
+]
