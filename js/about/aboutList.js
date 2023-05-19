001040000b1c38d71e04e1f1f63a42204310b548
Matthew Powell <matt.powell2784@gmail.com>
2023-05-19 11:54:20 +0100

    about list text updated

--- js/about/aboutList.js
+++ js/about/aboutList.js
@@ -3,18 +3,18 @@ export const aboutList = [
     key: 1,
     image: '/img/baby_robot.jpg',
     title: 'Meet Baby Robot',
-    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryapos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    text: "Baby Robot is an adorable AI companion designed to bring joy and wonder to people's lives. With its playful demeanor and advanced learning capabilities, Baby Robot learns and grows alongside its human friends, creating unforgettable memories in the futuristic world of 2050.",
   },
   {
     key: 2,
     image: '/img/gg_bot.jpg',
     title: 'Meet Generate G',
-    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryapos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    text: 'Generate G is an innovative AI bot known for its incredible creativity and problem-solving abilities. With its vast knowledge and cutting-edge algorithms, Generate G trains the human workforce at Agency 2050 by generating groundbreaking ideas and solutions, propelling society into a new era of innovation and progress.',
   },
   {
     key: 3,
     image: '/img/robot_man.png',
     title: 'Meet Mattbot 300',
-    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industryapos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    text: 'MattBot 300 is a highly efficient and reliable AI assistant at Agency 2050. With its meticulous attention to detail and lightning-fast processing power, MattBot 300 streamlines complex tasks, empowering our ai friends more time play time with their pet humans in the year 2050.',
   },
 ]
