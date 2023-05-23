93e190c8b0d656a7b973216dafa7774f18c34222
Matthew Powell <matt.powell2784@gmail.com>
2023-05-23 16:50:29 +0100

    add form submit logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 import { createAboutFlexItems } from './about/createAboutFlexItems.js'
+import { onFormSubmit } from './form/onFormSubmit.js'
 
 createAboutFlexItems()
+
+const form = document.getElementById('form')
+form.addEventListener('submit', onFormSubmit)
