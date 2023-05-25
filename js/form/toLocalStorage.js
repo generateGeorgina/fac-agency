d050b5945391addf34e429b31d9e63a7f8f3b39d
Georgina A <appeagyeigeorgina@gmail.com>
2023-05-25 13:58:52 +0100

    Add toLocalStorage js file

---
+++ js/form/toLocalStorage.js
@@ -0,0 +1,24 @@
+export function toLocalStorage() {
+    const form = document.getElementById('form').value;
+    const nameElement = document.getElementById("name").value;
+    const telephoneElement = document.getElementById("telephone").value;
+    const emailElement = document.getElementById("email").value;
+    const humanCountElement = document.getElementById("human-count").value;
+    const preferredEmailElement = document.getElementById("preferred-email").checked;
+    const preferredPhoneElement = document.getElementById("preferred-phone").checked;
+    const preferredPostElement = document.getElementById("preferred-post").checked;
+    const marketingConsentElement = document.getElementById("marketing-consent").checked;
+
+    const formDetails = {
+        "name": nameElement,
+        "telephone": telephoneElement,
+        "email": emailElement,
+        "humans": humanCountElement,
+        "emailRadio": preferredEmailElement,
+        "phoneRadio": preferredPhoneElement,
+        "postRadio": preferredPostElement,
+        "marketingConsent": marketingConsentElement
+    };
+
+    localStorage.setItem("formDetails", JSON.stringify(formDetails));
+}
